03240a10c978d2451534c573f6417b676064bd60
olearyc11 <157394769+olearyc11@users.noreply.github.com>
2024-04-23 16:06:16 -0400

    2nd  Commit

--- src/main/java/com/coderscampus/assignment8/Assignment8App.java
+++ src/main/java/com/coderscampus/assignment8/Assignment8App.java
@@ -1,11 +1,13 @@
 package com.coderscampus.assignment8;
 
+import java.util.List;
+
 public class Assignment8App {
 
 	public static void main(String[] args) {
 		
 		NumbersService numbersService = new NumbersService();
-		numbersService.getAllNumbers();
-		
+		List<Integer> numbers = numbersService.getListOfNumbers();
+		numbersService.printNumbersMap(numbers);
 	}
 }
--- src/main/java/com/coderscampus/assignment8/NumbersService.java
+++ src/main/java/com/coderscampus/assignment8/NumbersService.java
@@ -1,25 +1,45 @@
 package com.coderscampus.assignment8;
 
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
 
 public class NumbersService {
 
 	ExecutorService executor = Executors.newCachedThreadPool();
 	Assignment8 assignment = new Assignment8();
-	
-	public List<CompletableFuture<List<Integer>>> getAllNumbers() {
+
+	public List<Integer> getListOfNumbers() {
 		List<CompletableFuture<List<Integer>>> numTasks = new ArrayList<>();
 		for (int i = 0; i < 1000; i++) {
 			CompletableFuture<List<Integer>> numTask = CompletableFuture.supplyAsync(() -> assignment.getNumbers(), executor);
 			numTasks.add(numTask);
 		}
-		return numTasks;
+//		CompletableFuture<Void> allFutures = CompletableFuture.allOf(numTasks.toArray(new CompletableFuture[numTasks.size()]));
+//		allFutures.join();
+		List<Integer> singleList =  numTasks.stream()
+											.flatMap(task -> task.join().stream())
+											.collect(Collectors.toList());
+
+			return singleList;
+		
 	}
+
+	public void printNumbersMap(List<Integer> singleList) {
+		Map<Integer, Long> numbersMap = singleList.stream()
+				  								  .collect(Collectors.groupingBy(number -> number, Collectors.counting()));
+		String entryList = numbersMap.entrySet().stream()
+							 .map(entry -> entry.getKey() + "=" + entry.getValue())
+							 .collect(Collectors.joining(","));
+		System.out.println(entryList);
+							 
 	
-	
-	
+	}
+
 }
